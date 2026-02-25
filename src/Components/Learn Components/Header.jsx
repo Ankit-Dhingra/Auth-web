0e504f19fb4279ac3037bd31a3901d89cee18049
ankit dhingra <ankit.dhingra@taxmanntechnologies.com>
2026-02-25 16:08:52 +0530

    SAS

--- src/Components/Learn Components/Header.jsx
+++ src/Components/Learn Components/Header.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+
+  const [search, setSearch] = useState('');
+
+  const searchSuggestion = ['Bitcoin', 'Ethereum', 'Tether', 'Solana', 'Cardano', 'Polkadot'];
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  }
   return (
     <div className="relative bg-black flex items-center border-b border-white/20 shadow-md px-6 py-4">
 
@@ -14,6 +22,7 @@ const Header = () => {
         <input
           type="text"
           placeholder="Search Coin Here..."
+          value={search}
           className="
             w-80
             px-4 py-2
@@ -27,7 +36,22 @@ const Header = () => {
             focus:border-red-400
             transition
           "
+
+          onChange={(e) => handleSearch(e)}
         />
+      </div>
+      <div>
+        {
+          search.length > 0 && (
+            <div className="absolute left-1/2 -translate-x-1/2 top-16 bg-black/10 backdrop-blur-xl border border-white/20 w-80 px-4 py-2 rounded-lg">
+              {
+                searchSuggestion.filter((coin) => coin.toLowerCase().includes(search.toLowerCase())).map((coin) => (
+                  <p className="text-white text-sm py-2" key={coin}>{coin}</p>
+                ))
+              }
+            </div>
+          )
+        }
       </div>
 
     </div>
